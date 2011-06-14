9e12e778aa89bf2b4c57b747b7b3bdf7e13a9f16
Sugarat <Jeanlu@gmail.com>
2011-06-14 13:43:25 -0400

    Fixed Encrypted passwords

--- spec/models/user_spec2.rb
+++ spec/models/user_spec2.rb
@@ -11,9 +11,11 @@ describe User do
     }
   end
 
-  it "should create a new instance given valid attributes" do
-    User.create!(@attr)
-  end
+  describe "password encryption" do
+
+    before(:each) do
+      @user = User.create!(@attr)
+    end
 
   it "should create a new instance given valid attributes" do
     User.create!(@attr)
@@ -48,21 +50,13 @@ describe User do
     end
   end
 
-  it "should reject duplicate email addresses" do
-    # Put a user with given email address into the database.
-    User.create!(@attr)
-    user_with_duplicate_email = User.new(@attr)
-    user_with_duplicate_email.should_not be_valid
-  end
-
   it "should reject email addresses identical up to case" do
     upcased_email = @attr[:email].upcase
     User.create!(@attr.merge(:email => upcased_email))
     user_with_duplicate_email = User.new(@attr)
     user_with_duplicate_email.should_not be_valid
   end
-
-  describe "password validations" do
+describe "password validations" do
 
     it "should require a password" do
       User.new(@attr.merge(:password => "", :password_confirmation => "")).
@@ -85,17 +79,9 @@ describe User do
       hash = @attr.merge(:password => long, :password_confirmation => long)
       User.new(hash).should_not be_valid
     end
-  end
-
- describe "password encryption" do
-
-    before(:each) do
-      @user = User.create!(@attr)
-    end
 
     it "should have an encrypted password attribute" do
       @user.should respond_to(:encrypted_password)
     end
   end
 end
-
