c9d659b6f963dd5407a46d20f4a6a8b26cfd6824
Sugarat <Jeanlu@gmail.com>
2011-06-16 01:10:47 -0400

    Yet another install

---
+++ app/models/user.rb
@@ -0,0 +1,32 @@
+# == Schema Information
+# Schema version: 20110604190707
+#
+# Table name: users
+#
+#  id         :integer         not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
+class User < ActiveRecord::Base
+  attr_accessor :password
+  attr_accessible :name, :email, :password, :password_confirmation
+
+  email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  
+  validates :name,  :presence => true,
+                    :length   => { :maximum => 50 }
+  validates :email, :presence   => true,
+                    :format     => { :with => email_regex },
+                    :uniqueness => { :case_sensitive => false }
+  # Automatically create the virtual attribute 'password_confirmation'.
+  validates :password, :presence     => true,
+                       :confirmation => true,
+                       :length       => { :within => 6..40 }
+end
+
+
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '711de64aaa9b9bbb08d2094e75e101aac0f7216a25b90447ae550e75bdcd5dfdf435e403ac990b3813f2f90783487a1522c965c48006f0fe102ac9dcf1d5ca30'
